9cac97489bf1194bcb877798da49cf693f8078af
suraj1607 <suraj1877patil@hotmail.com>
2025-04-09 15:23:14 +0530

    tags in cucumber

--- src/test/java/features/firstProgram.feature
+++ src/test/java/features/firstProgram.feature
@@ -1,8 +1,46 @@
 Feature: Application Login
 
+  @Regression
   Scenario: Admin Page Default Login
     Given User is on Netbanking landing page
-    When User login into application
+    When User login into application with "admin" and password "1234"
     Then Home Page is displayed
     And Cards are displayed
+
+
+#  Scenario: User Page Default Login
+#    Given User is on Netbanking landing page
+#    When User login into application with "user" and password "5678"
+#    Then Home Page is displayed
+#    And Cards are displayed
+
+  @SmokeTest
+    @Regression
+  Scenario Outline: User Page Default Login
+    Given User is on Netbanking landing page
+    When User login into application with "<Username>" and password "<Password>"
+    Then Home Page is displayed
+    And Cards are displayed
+
+    Examples:
+      | Username  | Password  |
+      | debitUser | Debit1234 |
+      | stockUser | stock1234 |
+
+
+  @SmokeTest
+  Scenario: User Page Default Signup
+    Given User is on Practice Landing Page
+    When User SignUp into application
+      | suraj          |
+      | patil          |
+      | test@gmail.com |
+      | 9833403409     |
+    Then Home Page is displayed
+    And Cards are displayed
+
+
+
+
+
     
--- src/test/java/stepDefinations/MainSteps.java
+++ src/test/java/stepDefinations/MainSteps.java
@@ -4,15 +4,22 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+
 public class MainSteps {
     @Given("User is on Netbanking landing page")
     public void user_is_on_netbanking_landing_page() {
         System.out.println("User is on netbanking landing page");
     }
 
-    @When("User login into application")
-    public void user_login_into_application() {
-        System.out.println("User login into application");
+//    @When("User login into application with {string} and password {string}")
+//    public void user_login_into_application(String user, String password) {
+//        System.out.println("User login into application " + user + " " + password);
+//    }
+
+    @When("^User login into application with (.+) and password (.+)$")
+    public void user_login_into_application(String user, String password) {
+        System.out.println("User login into application " + user + " " + password);
     }
 
     @Then("Home Page is displayed")
@@ -23,6 +30,20 @@ public class MainSteps {
     @Then("Cards are displayed")
     public void cards_are_displayed() {
         System.out.println("Cards are Displayed");
+    }
+
+
+    @Given("User is on Practice Landing Page")
+    public void user_is_on_practice_landing_page() {
+        System.out.println("User is on Practice Landing Page");
+    }
+
+    @When("User SignUp into application")
+    public void user_sign_up_into_application(List<String> data) {
+        System.out.println("Name: " + data.getFirst());
+        System.out.println("Last Name: " + data.get(1));
+        System.out.println("Email: " + data.get(2));
+        System.out.println("Number: " + data.getLast());
     }
 
 }
